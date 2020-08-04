bae22c329ae34d24dd39f1b6633deb9d257ef9bd
Taylor Chen <s68175@gmail.com>
2020-08-04 22:15:54 +0800

    Express init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log('App is running on http://localhost:3000')
+})
