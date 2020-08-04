5988b91f4073b3267375a08fc6b86dac4e3f8d50
Taylor Chen <s68175@gmail.com>
2020-08-05 02:43:08 +0800

    Add show whole expense

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const Handlebars = require("handlebars")
+
+require('./config/mongoose')
 
 const app = express()
 
 const port = 3000
+
+Handlebars.registerHelper("categorySame", function (category, value, options) {
+  if (category === value) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+})
+
+const Record = require('./models/record')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -4,7 +4,7 @@ const db = require('../../config/mongoose.js')
 const categories = [
   { name: '家居物業', icon: '<i class="fas fa-home"></i>' },
   { name: '交通出行', icon: '<i class="fas fa-shuttle-van"></i>' },
-  { name: '休閒娛樂', icon: '<i class="fa-grin-beam"></i>' },
+  { name: '休閒娛樂', icon: '<i class="fas fa-grin-beam"></i>' },
   { name: '餐飲食品', icon: '<i class="fas fa-utensils"></i>' },
   { name: '其他', icon: '<i class="fas fa-pen"></i>' }]
 
