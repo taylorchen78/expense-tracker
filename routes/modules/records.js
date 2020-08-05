24f807327bed69d3613f48ecd3617b6e48f9a4af
Taylor Chen <s68175@gmail.com>
2020-08-06 02:52:47 +0800

    Add delete expense

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -62,4 +62,13 @@ router.put('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+
+  Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
