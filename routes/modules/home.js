c330b36697ecb528492a08fbfad791fbae4e3bf0
Taylor Chen <s68175@gmail.com>
2020-08-06 23:54:25 +0800

    Add total amount count

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,11 +2,27 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
+  const categoryList = []
+
+  Category.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(categories => {
+      categories.forEach(category => {
+        categoryList.push({ name: category.name })
+      })
+    })
+    .catch(error => console.error(error))
+
   Record.find()
     .lean()
-    .then(records => res.render('index', { records }))
+    .then(records => {
+      let totalAmount = records.map(record => record.amount).reduce((a, b) => a + b)
+      res.render('index', { records, categoryList, totalAmount })
+    })
     .catch(error => console.error(error))
 })
 
