3a578f2016c8c9e50ffbb450dd64202150fcbaf8
Taylor Chen <s68175@gmail.com>
2020-08-07 14:43:53 +0800

    Add db close

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -29,17 +29,4 @@ db.once('open', () => {
       db.close()
     })
     .catch(error => console.error(error))
-
-
-  // for (let i = 0; i < 10; i++) {
-  //   Record.create({
-  //     name: 'name-' + i,
-  //     date: '2020/' + numToStr(12) + '/' + numToStr(28),
-  //     category: Math.floor(Math.random() * 5),
-  //     amount: Math.floor(Math.random() * 1000)
-  //   })
-  // }
-
-  // console.log('done')
-  // db.close()
 })
