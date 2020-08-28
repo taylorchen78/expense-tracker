62a7a1b5105bb1b44e4aad40197608caf4e4f959
Taylor Chen <s68175@gmail.com>
2020-08-28 23:46:18 +0800

    Add login/register UI

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
 const filter = require('./modules/filter')
+const users = require('./modules/users')
 
-router.use('/', home)
 router.use('/records', records)
 router.use('/filter', filter)
+router.use('/users', users)
+router.use('/', home)
 
 module.exports = router
