35648a08253fb44d5a69025efe0f13e2fe87f3b5
Taylor Chen <s68175@gmail.com>
2020-08-29 22:07:12 +0800

    Add expense seeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,7 +1,16 @@
 const Record = require('../record')
+const User = require('../user')
 const db = require('../../config/mongoose.js')
+const bcrypt = require('bcryptjs')
 
 const initRecords = []
+
+const SEED_USER = {
+  name: 'Tony Stark',
+  email: 'tony@example.com',
+  password: '1234',
+}
+
 
 function numToStr(num) {
   let numStr = Math.ceil(Math.random() * num).toString()
@@ -14,19 +23,28 @@ function numToStr(num) {
 }
 
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    initRecords.push({
-      name: 'name-' + i,
-      date: '2020/' + numToStr(12) + '/' + numToStr(28),
-      category: Math.floor(Math.random() * 5),
-      amount: Math.floor(Math.random() * 1000)
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from({ length: 10 }, (_, i) =>
+        Record.create({
+          name: 'name-' + i,
+          date: '2020/' + numToStr(12) + '/' + numToStr(28),
+          category: Math.floor(Math.random() * 5),
+          amount: Math.floor(Math.random() * 1000),
+          userId: userId
+        })
+      ))
     })
-  }
-
-  Record.insertMany(initRecords)
     .then(() => {
-      console.log('Records are created')
-      db.close()
+      console.log('Create record seeds done.')
+      process.exit()
     })
-    .catch(error => console.error(error))
 })
