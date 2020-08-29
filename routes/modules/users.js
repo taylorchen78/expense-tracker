9177353227eb036a828a0752e53337e12e7ed708
Taylor Chen <s68175@gmail.com>
2020-08-29 10:21:11 +0800

    Add register account

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  if (password !== confirmPassword) {
+    return res.render('register', { name, email, password, confirmPassword })
+  }
+
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already register!')
+      res.render('register', { name, email, password, confirmPassword })
+    } else {
+      return User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
 })
 
 module.exports = router
